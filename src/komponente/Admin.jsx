e463e5b89781cbe5e5bbca717803f52c675be77a
Maja364 <majaaleksic62@gmail.com>
2023-02-19 18:29:53 +0100

    omogucena pretraga po autoru

--- src/komponente/Admin.jsx
+++ src/komponente/Admin.jsx
@@ -53,13 +53,42 @@ function Admin({dokumenta}) {
     function vratiLink(fileName){
       return "http://127.0.0.1:8000/uploads/"+fileName
     }
+    function searchTable() {
+      // uzimamo vrednost iz polja za pretragu
+      var input = document.getElementById("searchInput").value.toUpperCase();
+      
+      // pronalazimo tabelu i sve redove u njoj
+      var table = document.getElementById("myTable");
+      var rows = table.getElementsByTagName("tr");
+      
+      // prolazimo kroz svaki red u tabeli
+      for (var i = 0; i < rows.length; i++) {
+        var cells = rows[i].getElementsByTagName("td");
+        var found = false;
+        // prolazimo kroz svaku ćeliju u trenutnom redu
+        for (var j = 0; j < cells.length; j++) {
+          var cell = cells[j];
+          // ako sadrži traženu vrednost, postavljamo found na true i prekidamo petlju
+          if (cell.innerHTML.toUpperCase().indexOf(input) > -1) {
+            found = true;
+            break;
+          }
+        }
+        // prikazujemo ili skrivamo red u zavisnosti od toga da li smo pronašli traženu vrednost
+        if (found) {
+          rows[i].style.display = "";
+        } else {
+          rows[i].style.display = "none";
+        }
+      }
+    }
     return ( 
     <>
         <div className="container">
             <button className='btn btn-primary' onClick={handleLogout}> Odjavi se</button>
+            <input type="text"  id="searchInput" onChange={searchTable}/>
 
-
-            <table class="table table-dark">
+            <table class="table table-dark" >
               <thead>
                 <tr>
                   <th scope="col">ID</th>
@@ -70,7 +99,7 @@ function Admin({dokumenta}) {
 
                 </tr>
               </thead>
-              <tbody>
+              <tbody id="myTable">
                     {
 
                       dokumenta.map((d)=><tr key={d.id}>
